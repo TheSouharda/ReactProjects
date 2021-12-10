f535481851c60d1694b9103a6344fb94a4870246
Souharda <mail2souharda2015@gmail.com>
2021-12-10 11:53:52 +0530

    Components added

---
+++ expense-tracker/src/components/Balance.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Balance = () => {
+  return (
+    <div>
+      <h4>your Balance</h4>
+      <h1 id="balance">$10.00</h1>
+    </div>
+  );
+};
+
+export default Balance;
